de3fd55d66125d60447a1b905fc8c0eaa503e0f8
Developerr18 <dineshnikum1@gmail.com>
2025-06-10 21:48:22 +0530

    use data from backend for products

--- e-commerce-project/src/pages/HomePage.jsx
+++ e-commerce-project/src/pages/HomePage.jsx
@@ -1,13 +1,17 @@
 import axios from "axios";
+import { useState, useEffect } from "react";
 import Header from "../components/Header";
 import "./HomePage.css";
 import checkmark from "../assets/images/icons/checkmark.png";
-import { products } from "../starting-code/data/products";
 
 export default function HomePage() {
-    axios
-        .get("http://localhost:3000/api/products")
-        .then((response) => console.log(response.data));
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:3000/api/products")
+            .then((response) => setProducts(response.data));
+    }, []);
 
     return (
         <>
