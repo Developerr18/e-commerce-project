af22cb5e09dd0254767670dcc4f01348536b468e
Developerr18 <dineshnikum1@gmail.com>
2025-06-10 21:56:01 +0530

    get cart from backend

--- e-commerce-project/src/components/Header.jsx
+++ e-commerce-project/src/components/Header.jsx
@@ -5,7 +5,13 @@ import mobileWhiteLogo from "../assets/images/mobile-logo-white.png";
 import cartIcon from "../assets/images/icons/cart-icon.png";
 import searchIcon from "../assets/images/icons/search-icon.png";
 
-export default function Header() {
+export default function Header({ cart }) {
+    let totalQuantity = 0;
+
+    cart.forEach((cartItem) => {
+        totalQuantity += cartItem.quantity;
+    });
+
     return (
         <div className="header">
             <div className="left-section">
@@ -34,7 +40,7 @@ export default function Header() {
 
                 <NavLink className="cart-link header-link" to="/checkout">
                     <img className="cart-icon" src={cartIcon} />
-                    <div className="cart-quantity">3</div>
+                    <div className="cart-quantity">{totalQuantity}</div>
                     <div className="cart-text">Cart</div>
                 </NavLink>
             </div>
