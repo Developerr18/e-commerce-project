a2807604731b05574c382a157b16d5f70134e013
Developerr18 <dineshnikum1@gmail.com>
2025-06-11 18:33:42 +0530

    seprate components

--- e-commerce-project/src/App.jsx
+++ e-commerce-project/src/App.jsx
@@ -2,10 +2,10 @@ import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router";
 import axios from "axios";
 import "./App.css";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/Home/HomePage";
 import CheckoutPage from "./pages/checkout/CheckoutPage";
-import OrdersPage from "./pages/OrdersPage";
-import TrackingPage from "./pages/TrackingPage";
+import OrdersPage from "./pages/orders/OrdersPage";
+import TrackingPage from "./pages/tracking/TrackingPage";
 import ErrorPage from "./pages/ErrorPage";
 
 function App() {
@@ -21,7 +21,7 @@ function App() {
         <Routes>
             <Route index element={<HomePage cart={cart} />} />
             <Route path="checkout" element={<CheckoutPage cart={cart} />} />
-            <Route path="orders" element={<OrdersPage />} />
+            <Route path="orders" element={<OrdersPage cart={cart} />} />
             <Route path="tracking" element={<TrackingPage />} />
             <Route path="*" element={<ErrorPage />} />
         </Routes>
