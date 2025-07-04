ff2df27060682076a4592433c1b50e741e3507bd
Developerr18 <dineshnikum1@gmail.com>
2025-07-04 22:40:20 +0530

    add

--- react-dev-exercises/src/managing-state/State-manage.jsx
+++ react-dev-exercises/src/managing-state/State-manage.jsx
@@ -542,7 +542,7 @@ const contacts = [
 */
 
 ///////////////////////////////////////////////
-
+/*
 export default function App() {
     const [reverse, setReverse] = useState(false);
     let checkbox = (
@@ -558,16 +558,16 @@ export default function App() {
     if (reverse) {
         return (
             <>
-                <Field label="Last name" />
-                <Field label="First name" />
+                <Field key="lastName" label="Last name" />
+                <Field key="firstName" label="First name" />
                 {checkbox}
             </>
         );
     } else {
         return (
             <>
-                <Field label="First name" />
-                <Field label="Last name" />
+                <Field key="firstName" label="First name" />
+                <Field key="lastName" label="Last name" />
                 {checkbox}
             </>
         );
@@ -589,3 +589,117 @@ function Field({ label }) {
         </label>
     );
 }
+*/
+
+///////////////////////////////////////////////
+
+export default function ContactManager() {
+    const [contacts, setContacts] = useState(initialContacts);
+    const [selectedId, setSelectedId] = useState(0);
+    const selectedContact = contacts.find((c) => c.id === selectedId);
+
+    function handleSave(updatedData) {
+        const nextContacts = contacts.map((c) => {
+            if (c.id === updatedData.id) {
+                return updatedData;
+            } else {
+                return c;
+            }
+        });
+        setContacts(nextContacts);
+    }
+
+    return (
+        <div>
+            <ContactList
+                contacts={contacts}
+                selectedId={selectedId}
+                onSelect={(id) => setSelectedId(id)}
+            />
+            <hr />
+            <EditContact
+                key={selectedContact.id}
+                initialData={selectedContact}
+                onSave={handleSave}
+            />
+        </div>
+    );
+}
+
+function ContactList({ contacts, selectedId, onSelect }) {
+    return (
+        <section>
+            <ul>
+                {contacts.map((contact) => (
+                    <li key={contact.id}>
+                        <button
+                            onClick={() => {
+                                onSelect(contact.id);
+                            }}
+                        >
+                            {contact.id === selectedId ? (
+                                <b>{contact.name}</b>
+                            ) : (
+                                contact.name
+                            )}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
+}
+
+function EditContact({ initialData, onSave }) {
+    const [name, setName] = useState(initialData.name);
+    const [email, setEmail] = useState(initialData.email);
+
+    return (
+        <section>
+            <label>
+                Name:{" "}
+                <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </label>
+            <br />
+            <label>
+                Email:{" "}
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+            </label>
+            <br />
+            <button
+                onClick={() => {
+                    const updatedData = {
+                        id: initialData.id,
+                        name: name,
+                        email: email,
+                    };
+                    onSave(updatedData);
+                }}
+            >
+                Save
+            </button>
+            <button
+                onClick={() => {
+                    setName(initialData.name);
+                    setEmail(initialData.email);
+                }}
+            >
+                Reset
+            </button>
+        </section>
+    );
+}
+
+const initialContacts = [
+    { id: 0, name: "Taylor", email: "taylor@mail.com" },
+    { id: 1, name: "Alice", email: "alice@mail.com" },
+    { id: 2, name: "Bob", email: "bob@mail.com" },
+];
