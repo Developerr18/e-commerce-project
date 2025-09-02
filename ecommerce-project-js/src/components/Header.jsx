a0eecae6192ad9326a40266ee4ddb483ee1df269
Developerr18 <dineshnikum1@gmail.com>
2025-09-03 03:33:50 +0530

    update

--- ecommerce-project-js/src/components/Header.jsx
+++ ecommerce-project-js/src/components/Header.jsx
@@ -7,62 +7,57 @@ import searchIcon from "../assets/images/icons/search-icon.png";
 import { useState } from "react";
 
 export default function Header({ cart }) {
-    const navigate = useNavigate();
-    const [searchParams] = useSearchParams();
-    const searchText = searchParams.get("search");
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const searchText = searchParams.get("search");
 
-    const [search, setSearch] = useState(searchText || "");
+  const [search, setSearch] = useState(searchText || "");
 
-    // handle search-bar and 'search' button
-    const updateSearchInput = (e) => {
-        setSearch(e.target.value);
-    };
+  const searchProducts = () => {
+    navigate(`/?search=${search}`);
+  };
 
-    const searchProducts = () => {
-        navigate(`/?search=${search}`);
-    };
+  // get total cart quantity
+  let totalQuantity = 0;
 
-    // get total cart quantity
-    let totalQuantity = 0;
+  cart.forEach((cartItem) => {
+    totalQuantity += cartItem.quantity;
+  });
 
-    cart.forEach((cartItem) => {
-        totalQuantity += cartItem.quantity;
-    });
+  return (
+    <div className="header">
+      <div className="left-section">
+        <NavLink to="/" className="header-link">
+          <img className="logo" src={whiteLogo} />
+          <img className="mobile-logo" src={mobileWhiteLogo} />
+        </NavLink>
+      </div>
 
-    return (
-        <div className="header">
-            <div className="left-section">
-                <NavLink to="/" className="header-link">
-                    <img className="logo" src={whiteLogo} />
-                    <img className="mobile-logo" src={mobileWhiteLogo} />
-                </NavLink>
-            </div>
+      <div className="middle-section">
+        <input
+          className="search-bar"
+          type="text"
+          placeholder="Search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
 
-            <div className="middle-section">
-                <input
-                    className="search-bar"
-                    type="text"
-                    placeholder="Search"
-                    value={search}
-                    onChange={updateSearchInput}
-                />
+        <button className="search-button" onClick={searchProducts}>
+          <img className="search-icon" src={searchIcon} />
+        </button>
+      </div>
 
-                <button className="search-button" onClick={searchProducts}>
-                    <img className="search-icon" src={searchIcon} />
-                </button>
-            </div>
+      <div className="right-section">
+        <NavLink className="orders-link header-link" to="/orders">
+          <span className="orders-text">Orders</span>
+        </NavLink>
 
-            <div className="right-section">
-                <NavLink className="orders-link header-link" to="/orders">
-                    <span className="orders-text">Orders</span>
-                </NavLink>
-
-                <NavLink className="cart-link header-link" to="/checkout">
-                    <img className="cart-icon" src={cartIcon} />
-                    <div className="cart-quantity">{totalQuantity}</div>
-                    <div className="cart-text">Cart</div>
-                </NavLink>
-            </div>
-        </div>
-    );
+        <NavLink className="cart-link header-link" to="/checkout">
+          <img className="cart-icon" src={cartIcon} />
+          <div className="cart-quantity">{totalQuantity}</div>
+          <div className="cart-text">Cart</div>
+        </NavLink>
+      </div>
+    </div>
+  );
 }
